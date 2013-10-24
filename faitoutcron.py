40348e62af6fa618c68838f734694e26152eb41a
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-10-24 11:09:12 +0200

    Add some command line arguments to the cron job

--- faitoutcron.py
+++ faitoutcron.py
@@ -35,22 +35,42 @@ You may set it up as:
 """
 
 
-
 __requires__ = ['SQLAlchemy >= 0.7', 'jinja2 >= 2.4']
 import pkg_resources
 import os
 
 from datetime import datetime
 from datetime import timedelta
+from optparse import OptionParser
 
 from faitout import APP, SESSION, ADMIN_ENGINE, faitoutlib
 
-def run_cron():
+
+def get_arguments():
+    """ Set and retrieve the command line arguments. """
+    parser = OptionParser()
+    parser.add_option("-d", "--debug", dest="debug", action="store_true",
+                      default=False,
+                      help="Print some information while running")
+    parser.add_option("-t", "--test", dest="test", action="store_true",
+                      default=False,
+                      help="Print the connections to be removed, but do "
+                           "not remove them")
+
+    (options, args) = parser.parse_args()
+    return options
+
+
+def run_cron(debug=False, test=False):
     """ Runs the cleaning cron job.
 
-    This jobs retrieves from the configuration file the interval at which it is
-    ran. It then retrieves a list of all connections which are older than this
-    interval and simply deletes those connections.
+    This jobs retrieves from the configuration file the interval at which
+    it is ran. It then retrieves a list of all connections which are older
+    than this interval and simply deletes those connections.
+
+    :kwarg debug: print some output while dropping the connections.
+    :kwarg test: just print the connections that will be drop but do not
+        do anything else.
 
     """
     interval = APP.config['CRON_FREQUENCY']
@@ -59,18 +79,22 @@ def run_cron():
     connections = faitoutlib.model.Connection.older_than(SESSION, limit)
 
     for connection in connections:
-        print connection
+        if debug or test:
+            print connection
         try:
-            output = faitoutlib.drop_connection(
-                SESSION,
-                ADMIN_ENGINE,
-                connection.connection_ip,
-                connection.connection_db_name,
-            )
+            if not test:
+                output = faitoutlib.drop_connection(
+                    SESSION,
+                    ADMIN_ENGINE,
+                    connection.connection_ip,
+                    connection.connection_db_name,
+                )
         except faitoutlib.FaitoutException as err:
             print 'Error while deleting %s' % connection.connection_db_name
             print >> sys.stderr, err
 
 
 if __name__ == '__main__':
-    run_cron()
+    args = get_arguments()
+    print args
+    run_cron(debug=args.debug, test=args.test)
