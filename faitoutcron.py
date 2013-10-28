9c76ec2e8ee2ef1da8c5df5d1ba4773cd435ff24
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-10-28 10:36:02 +0100

    Add missing import

--- faitoutcron.py
+++ faitoutcron.py
@@ -38,6 +38,7 @@ You may set it up as:
 __requires__ = ['SQLAlchemy >= 0.7', 'jinja2 >= 2.4']
 import pkg_resources
 import os
+import sys
 
 from datetime import datetime
 from datetime import timedelta
